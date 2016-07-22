90f44ca6666431141f5ebe29562a1333d71f7e17
Marco <marco1@live.de>
2016-07-23 01:17:03 +0200

    Change the license from 'Apache License 2.0' to 'MIT License'

---
+++ Migration.md
@@ -0,0 +1,5 @@
+# Migration
+
+## From `v2.x.x` to `v3.x.x`
+
+ * The license has been changed from the [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0) to the [MIT License](https://opensource.org/licenses/MIT).
--- Source/library/src/main/java/im/delight/android/webview/AdvancedWebView.java
+++ Source/library/src/main/java/im/delight/android/webview/AdvancedWebView.java
@@ -1,19 +1,9 @@
 package im.delight.android.webview;
 
 /*
- * Copyright (c) delight.im <info@delight.im>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Android-AdvancedWebView (https://github.com/delight-im/Android-AdvancedWebView)
+ * Copyright (c) delight.im (https://www.delight.im/)
+ * Licensed under the MIT License (https://opensource.org/licenses/MIT)
  */
 
 import android.view.ViewGroup;
