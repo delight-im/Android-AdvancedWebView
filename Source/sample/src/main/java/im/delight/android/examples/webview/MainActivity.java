ea3fe23eff975ab3773d2132232dbbc14b8eb9d0
dtdaft <30375420+dtdaft@users.noreply.github.com>
2017-07-27 19:18:32 +0200

    Update MainActivity.java
    import fix

--- Source/sample/src/main/java/im/delight/android/examples/webview/MainActivity.java
+++ Source/sample/src/main/java/im/delight/android/examples/webview/MainActivity.java
@@ -11,6 +11,8 @@ import android.content.Intent;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity implements AdvancedWebView.Listener {
 
