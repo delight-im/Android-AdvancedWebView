cf7f6b78a014d99dc24b7d66beaa2ba02a1000a8
Marco <marco@delight.im>
2018-02-17 07:06:38 +0100

    Remove 'onUnhandledInputEvent' from 'WebViewClient' and its usages

--- Source/library/src/main/java/im/delight/android/webview/AdvancedWebView.java
+++ Source/library/src/main/java/im/delight/android/webview/AdvancedWebView.java
@@ -645,19 +645,6 @@ public class AdvancedWebView extends WebView {
 				}
 			}
 
-			@SuppressLint("NewApi")
-			@SuppressWarnings("all")
-			public void onUnhandledInputEvent(WebView view, InputEvent event) {
-				if (Build.VERSION.SDK_INT >= 21) {
-					if (mCustomWebViewClient != null) {
-						mCustomWebViewClient.onUnhandledInputEvent(view, event);
-					}
-					else {
-						super.onUnhandledInputEvent(view, event);
-					}
-				}
-			}
-
 			@Override
 			public void onScaleChanged(WebView view, float oldScale, float newScale) {
 				if (mCustomWebViewClient != null) {
