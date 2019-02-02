4f205cf281ab6778a821b0b2d72912fa49c0ef86
Taco <SkytkRSfan3895@gmail.com>
2019-02-01 20:52:30 -0500

    (3/4) sample app updates: update to android pie, bump minSdk to 14, compile with java 1.8, add new default icon (plus adaptive)
    Signed-off-by: Taco <SkytkRSfan3895@gmail.com>

--- Source/sample/src/main/java/im/delight/android/examples/webview/MainActivity.java
+++ Source/sample/src/main/java/im/delight/android/examples/webview/MainActivity.java
@@ -1,16 +1,16 @@
 package im.delight.android.examples.webview;
 
-import im.delight.android.webview.AdvancedWebView;
-import android.webkit.WebChromeClient;
-import android.widget.Toast;
-import android.webkit.WebView;
-import android.view.View;
-import android.webkit.WebViewClient;
-import android.graphics.Bitmap;
-import android.content.Intent;
-import android.annotation.SuppressLint;
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import im.delight.android.webview.AdvancedWebView;
 
 public class MainActivity extends Activity implements AdvancedWebView.Listener {
 
@@ -22,7 +22,7 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		mWebView = (AdvancedWebView) findViewById(R.id.webview);
+		mWebView = findViewById(R.id.webview);
 		mWebView.setListener(this, this);
 		mWebView.setGeolocationEnabled(false);
 		mWebView.setMixedContentAllowed(true);
@@ -49,7 +49,6 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener {
 		mWebView.loadUrl(TEST_PAGE_URL);
 	}
 
-	@SuppressLint("NewApi")
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -57,7 +56,6 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener {
 		// ...
 	}
 
-	@SuppressLint("NewApi")
 	@Override
 	protected void onPause() {
 		mWebView.onPause();
@@ -81,7 +79,9 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener {
 
 	@Override
 	public void onBackPressed() {
-		if (!mWebView.onBackPressed()) { return; }
+		if (!mWebView.onBackPressed()) {
+			return;
+		}
 		// ...
 		super.onBackPressed();
 	}
@@ -98,24 +98,43 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener {
 
 	@Override
 	public void onPageError(int errorCode, String description, String failingUrl) {
-		Toast.makeText(MainActivity.this, "onPageError(errorCode = "+errorCode+",  description = "+description+",  failingUrl = "+failingUrl+")", Toast.LENGTH_SHORT).show();
+		Toast.makeText(MainActivity.this,
+				"onPageError(errorCode = " + errorCode
+						+ ",  description = " + description
+						+ ",  failingUrl = " + failingUrl
+						+ ")",
+				Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
-	public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
-		Toast.makeText(MainActivity.this, "onDownloadRequested(url = "+url+",  suggestedFilename = "+suggestedFilename+",  mimeType = "+mimeType+",  contentLength = "+contentLength+",  contentDisposition = "+contentDisposition+",  userAgent = "+userAgent+")", Toast.LENGTH_LONG).show();
+	public void onDownloadRequested(String url, String suggestedFilename,
+									String mimeType, long contentLength,
+									String contentDisposition, String userAgent) {
+		Toast.makeText(MainActivity.this,
+				"onDownloadRequested(url = " + url
+						+ ",  suggestedFilename = " + suggestedFilename
+						+ ",  mimeType = " + mimeType
+						+ ",  contentLength = " + contentLength
+						+ ",  contentDisposition = " + contentDisposition
+						+ ",  userAgent = " + userAgent
+						+ ")",
+				Toast.LENGTH_LONG).show();
 
-		/*if (AdvancedWebView.handleDownload(this, url, suggestedFilename)) {
+		/*
+		if (AdvancedWebView.handleDownload(this, url, suggestedFilename)) {
 			// download successfully handled
 		}
 		else {
 			// download couldn't be handled because user has disabled download manager app on the device
-		}*/
+		}
+		*/
 	}
 
 	@Override
 	public void onExternalPageRequest(String url) {
-		Toast.makeText(MainActivity.this, "onExternalPageRequest(url = "+url+")", Toast.LENGTH_SHORT).show();
+		Toast.makeText(MainActivity.this,
+				"onExternalPageRequest(url = " + url
+						+ ")",
+				Toast.LENGTH_SHORT).show();
 	}
-
 }
